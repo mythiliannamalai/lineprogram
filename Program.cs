f30c492ee90fd885b69445fdd05227471d691554
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-07 16:20:48 +0530

    Cal_two_line_distance

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Welcome to the line comparison camputation");
+﻿namespace lineprogram
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the line comparison camputation");
+            Console.WriteLine("enter x1 value : ");
+            var x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter x2 value : ");
+            var x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter y1 value : ");
+            var y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter y2 value : ");
+            var y2 = Convert.ToDouble(Console.ReadLine());
+            var a = Math.Pow(x2 - x1, 2);
+            var b= Math.Pow(y2 - y1, 2);
+            var distance = Math.Sqrt(a+b);
+            Console.WriteLine("distance of 2 point is : " + distance);
+        }
+    }
+}
